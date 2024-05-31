b3598012c16075a2c5fcd06aefc414c37c48d15a
Tuconsultor <ramondanielrivera@gmail.com>
2024-05-31 14:43:56 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por tu mensaje. Nos pondremos en contacto contigo pronto.');
+});
